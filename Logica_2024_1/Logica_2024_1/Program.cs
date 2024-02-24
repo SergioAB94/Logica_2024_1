c5559ab6fbc26e7b25c794ec2f43757d0c329630
SERGIO ANDRES ARREDONDO BURGOS <sergioarredondo297037@correo.itm.edu.co>
2024-02-24 00:10:18 -0500

    Actualizado el codigo de Logica_2024_1

---
+++ Logica_2024_1/Logica_2024_1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Logica_2024_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hola Mundo!");
+        }
+    }
+}
