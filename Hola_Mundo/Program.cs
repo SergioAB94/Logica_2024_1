1feb9628e70d729708d72f8854ab6b993e4a9a16
SERGIO ANDRES ARREDONDO BURGOS <sergioarredondo297037@correo.itm.edu.co>
2024-03-01 21:45:25 -0500

    Actulizado mi primer hola mundo

---
+++ Hola_Mundo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hola_Mundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
