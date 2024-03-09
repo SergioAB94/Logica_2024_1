26af0c9095b0b396b057f9a2baf2b9b1968990d1
SERGIO ANDRES ARREDONDO BURGOS <sergioarredondo297037@correo.itm.edu.co>
2024-03-08 20:24:44 -0500

    parte 1 ok

---
+++ Parcial_Punto_1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Parcial_Punto_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            double sueldobasico = 1000;
+            double venta1, venta2, venta3;
+
+
+            
+        }
+    }
+}
