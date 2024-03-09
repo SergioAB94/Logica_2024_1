89664e7324157227079983b11ad69a229fe14fa4
SERGIO ANDRES ARREDONDO BURGOS <sergioarredondo297037@correo.itm.edu.co>
2024-03-08 21:50:40 -0500

    parte 1

---
+++ Parcial_Punto_2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Parcial_Punto_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //definir variables:
+            double velocidadTren1 = 80;
+            double velocidadTren2 = 100;
+
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
