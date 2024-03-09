c7250356c2ec68ff2ce08d2b55f64b0da5a20cdd
SERGIO ANDRES ARREDONDO BURGOS <sergioarredondo297037@correo.itm.edu.co>
2024-03-08 20:34:50 -0500

    parte2

--- Parcial_Punto_1/Program.cs
+++ Parcial_Punto_1/Program.cs
@@ -8,6 +8,15 @@
             double sueldobasico = 1000;
             double venta1, venta2, venta3;
 
+            //cuanto valio cada venta
+            Console.WriteLine("ingrese el valor de venta1: ");
+            venta1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ingrese el valor de venta2: "); 
+            venta2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ingrese el valor de venta3: ");
+            venta3 = Convert.ToDouble(Console.ReadLine());
+
+            //Calcular el valor de las comisiones
 
             
         }
