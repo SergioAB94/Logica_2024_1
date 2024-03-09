29e1917b09f3c446923db9d6ea3b1c914af3e2a8
SERGIO ANDRES ARREDONDO BURGOS <sergioarredondo297037@correo.itm.edu.co>
2024-03-08 21:23:01 -0500

    perdido

--- Parcial_Punto_1/Program.cs
+++ Parcial_Punto_1/Program.cs
@@ -11,7 +11,7 @@
             //cuanto valio cada venta
             Console.WriteLine("ingrese el valor de venta1: ");
             venta1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("ingrese el valor de venta2: "); 
+            Console.WriteLine("ingrese el valor de venta2: ");
             venta2 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("ingrese el valor de venta3: ");
             venta3 = Convert.ToDouble(Console.ReadLine());
@@ -25,8 +25,24 @@
             double totalComisiones = comisionventa1 + comisionventa2 + comisionventa3;
             double sueldoTotal = comisionventa1 + comisionventa2 + comisionventa3 + sueldobasico;
 
+            Console.WriteLine(totalComisiones);
+            Console.WriteLine(sueldoTotal);
+
             //Calcular promedio de comisiones
             double promedioComisiones = totalComisiones / 3;
+            Console.WriteLine(promedioComisiones);
+
+            //calcular si gano el beneficio extra
+            string mensaje;
+            if (totalComisiones > 1000000)
+            {
+                mensaje= "ganaste el beneficio extra";
+            }
+            else
+            {
+                mensaje= "No alcanzaste el benficio extra";
+            }
+                
 
 
         }
