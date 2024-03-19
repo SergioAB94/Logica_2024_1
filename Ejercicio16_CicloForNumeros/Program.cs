1b61dfa9a53c10a42bb533a00700f149fb1f9a59
SERGIO ANDRES ARREDONDO BURGOS <sergioarredondo297037@correo.itm.edu.co>
2024-03-18 20:46:05 -0500

    Parte 1

---
+++ Ejercicio16_CicloForNumeros/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ejercicio16_CicloForNumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumuladorPar = 0, acumuladorImpar  = 0;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+
+                if (i % 2 == 0) acumuladorPar += i;
+                else acumuladorImpar += i;
+            }
+        }
+    }
+}
