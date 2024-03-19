44bb3c6519c983b06e8079929ef7c912b4283269
SERGIO ANDRES ARREDONDO BURGOS <sergioarredondo297037@correo.itm.edu.co>
2024-03-18 21:13:03 -0500

    Parte1

---
+++ Ejercicio14_NumerosIntervalos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ejercicio14_NumerosIntervalos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, numMenor, numMayor;
+            
+            Console.WriteLine("Ingrese el primer numero");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                numMenor = num2;
+                numMayor = num1;
+            }
+            else
+            {
+                numMenor = num1;
+                numMayor = num2;
+            }
+
+            for (int i = numMenor; i<numMayor; i++)
+            {
+
+            }
+        }
+
+
+
+        }
+    }
+}
