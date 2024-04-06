9ad310c4ecc76367beb5cb6b8e2cb29077db3eec
SERGIO ANDRES ARREDONDO BURGOS <sergioarredondo297037@correo.itm.edu.co>
2024-04-05 20:26:38 -0500

    parte 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cantidad de jugadores
+            int numJugadores = 0;
+
+            do
+            {
+                Console.WriteLine("Ingrese el numero de jugadores entre 2-4: ");
+                numJugadores = int.Parse(Console.ReadLine());
+            } while (numJugadores < 2 || numJugadores > 4);
+
+            //generar un numero al azar segun la cantidad de jugadores
+            int numMax = 0;
+        }
+    }
+}
