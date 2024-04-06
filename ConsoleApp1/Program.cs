f6cecc9ce31055bb5db3cff42cb5cfd39f3097b3
SERGIO ANDRES ARREDONDO BURGOS <sergioarredondo297037@correo.itm.edu.co>
2024-04-05 20:36:44 -0500

    parte 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,6 +15,27 @@
 
             //generar un numero al azar segun la cantidad de jugadores
             int numMax = 0;
+            switch (numJugadores)
+            {
+                case 1:
+                    numMax = 50;
+                    break;
+                case 2:
+                    numMax = 100;
+                    break;
+                case 3:
+                    numMax = 200;
+                    break;
+            }
+            Random random = new Random();
+            int numAzar = random.Next(numMax + 1);
+
+            //iniciar el juego
+            bool jugar = true;
+            while (jugar)
+            {
+                //turno 1
+            }
         }
     }
 }
