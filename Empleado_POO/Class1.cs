5b279c769baafecd221a6e014d7b37a96206fec2
SERGIO ANDRES ARREDONDO BURGOS <sergioarredondo297037@correo.itm.edu.co>
2024-04-26 20:40:28 -0500

    Primera parte

---
+++ Empleado_POO/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace Empleado_POO
+{
+    public class Empleado
+    {
+        //Properties
+        public string Nombre { get; set; }
+        public string Edad {  get; set; }
+        public string VentasMes {  get; set; }
+        public double ValorVenta {  get; set; }
+
+        //Methods 
+
+    }
+}
