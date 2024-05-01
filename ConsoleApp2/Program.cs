be89b279412586894f78be10d7214abfa4457b32
SERGIO ANDRES ARREDONDO BURGOS <sergioarredondo297037@correo.itm.edu.co>
2024-04-30 23:46:09 -0500

    Codigo VetCenter

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using Ejercicio36_POO_vet;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crear un programa con POO de un centro veterinario donde me muestre un menú con las siguientes opciones: Comprar, Adoptar, Regalar.
+
+            Si el usuario elige “Comprar”, entonces debe mostrar un menú con 5 razas diferentes para ser elegida por ese comprador, y luego mostrar la raza con su precio. (Usted es libre de elegir esas 5 razas y el valor de cada una de ellas)
+
+            Si el usuario elige “Adoptar”, debe pedir la raza y mostrar por pantalla un mensaje de agradecimiento por adoptar una mascota
+
+            Si el usuario elige “Regalar”, entonces pedir por pantalla el nombre de la mascota, edad, raza y peso que va a regalar. Mostrar un mensaje de agradecimient*/
+
+            //creamos la instancia de la clase Vet_Center
+            Vet_Center vet_Center = new Vet_Center();
+
+            //Aqui estamos invocando el metodo
+            vet_Center.ShowMenu();
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/Vet_Center.cs
@@ -0,0 +1,102 @@
+﻿namespace Ejercicio36_POO_vet
+{
+    public class Vet_Center
+    {
+        public Vet_Center()
+        {
+             
+        }
+
+        #region Methods 
+
+        public void ShowMenu()
+        {
+            //Declaracion de variables locales
+            int option;
+
+            Console.WriteLine("Bienvenido a Vet_Center");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Seleccione una opcion--");
+            Console.WriteLine("1. Comprar             ");
+            Console.WriteLine("2. Adoptar"             );
+            Console.WriteLine("3. Regalar             ");
+            option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1: //Comprar una mascota
+                    BuyPet();
+                    break;
+
+                case 2:// Adoptar mascota
+                    AdoptPet();
+                    break;
+
+                case 3:// Regalar mascota
+                    GivePet();
+                    break;
+
+            }
+        }
+
+        private void BuyPet()
+        {
+
+            Console.WriteLine("---Razas Disponibles----");
+            Console.WriteLine("----------------------- ");
+            Console.WriteLine("Seleccione una raza:--- ");
+            Console.WriteLine("1. PastorAleman $15.000 ");
+            Console.WriteLine("2. PitBull      $30.000 ");
+            Console.WriteLine("3. Salchicha    $25.000 ");
+            Console.WriteLine("3. Golden       $10.000 ");
+            Console.WriteLine("3. BullDog      $5.000  ");
+            int breedOption = Convert.ToInt32(Console.ReadLine());
+
+            switch (breedOption)
+            {
+                case 1:
+                    Console.WriteLine("Felicitaciones compraste un PastorAleman");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Felicitaciones compraste un PittBull");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Felicitaciones compraste un Salchicha");
+                    break;
+                case 4:
+                    Console.WriteLine("Felicitaciones compraste un Golden");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Felicitaciones compraste un BullDog");
+                    break;
+                }
+            }
+        private void AdoptPet()
+        {
+                Console.WriteLine("Ingrese la raza que desea adoptar");
+                string breed = Console.ReadLine();
+
+                Console.WriteLine(string.Format("Gracias por adoptar un {0}.", breed));
+        }
+        private void GivePet()
+        {
+                Console.Write("ingrese el nombre de la mascota");
+                string name = Console.ReadLine();   
+                Console.Write("ingrese la edad");
+                int age = Convert.ToInt32(Console.ReadLine());
+                Console.Write("ingrese la raza");
+                string breed = Console.ReadLine();
+                Console.Write("ingrese el peso");
+                double weight = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(string.Format(" gracias por regalar a {0}, de {1} años, de raza{2}, y con un peso de {3} Kg", name, age, breed, weight));
+        }
+
+
+
+        #endregion
+    }
+}
